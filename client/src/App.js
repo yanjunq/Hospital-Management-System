d4607c74ad130329284943e571537d7e1d239b46
yqa32 <yanjun_qian@sfu.ca>
2024-05-11 21:08:20 -0700

    initial project

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      }
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
+
---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({"user":["userOne", "userTwo","userThree"]})
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+})
+
